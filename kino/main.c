0cc2547d196100df4df7ff1cce06e42b6cb11858
Maciej Affeltowicz <sztajgerland@gmail.com>
2025-02-27 15:01:39 +0100

    first commit

--- kino/main.c
+++ kino/main.c
@@ -3,16 +3,12 @@
 #include "cJSON.c"
 #include "json_handler.h"
 
-
-
-
-
 int main() {
     movie_t movie;
     if (parse_json_to_struct("data.json", &movie) == 0) {
-        printf("Imie: %s\n", movie.title);
-        printf("Wiek: %u\n", movie.movie_duration);
-        printf("Email: %s\n", movie.genre);
+        printf("Title: %s\n", movie.title);
+        printf("Movie Duration: %u\n", movie.movie_duration);
+        printf("Genre: %s\n", movie.genre);
     } else {
         printf("Błąd podczas parsowania JSON.\n");
     }
---
+++ spis/book_mngr.c
@@ -0,0 +1,11 @@
+
+
+
+typedef struct  
+{
+    char author[30];
+    char title [30];
+    int year;
+} Book;
+
+
